590e9bb21dbf739eb5bafcb7ca20d9ba67960e64
yqtianust <yqtianust@gmail.com>
2021-07-20 23:59:40 +0800

    fix error in logger

--- myLogger.py
+++ myLogger.py
@@ -7,7 +7,7 @@ def create_logger(filepath, logger_name=None):
     if logger_name is None:
         logger_name = filepath
 
-    create_folder(os.path.dirname(filepath))
+    create_folder(os.path.dirname(filepath), False)
     logging.basicConfig(filename=filepath, filemode='a', level=logging.INFO, datefmt='%m-%d %H:%M:%S',
                         format='%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(logger_name)
